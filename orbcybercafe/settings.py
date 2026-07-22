c8c863723f265adc6bf2d98ebaa665749a006afd
evans <maingibiz002@gmail.com>
2026-07-22 09:52:28 +0300

    Add STATIC_ROOT for collectstatic build

--- orbcybercafe/settings.py
+++ orbcybercafe/settings.py
@@ -107,6 +107,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+# Add STATIC_ROOT right here:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
