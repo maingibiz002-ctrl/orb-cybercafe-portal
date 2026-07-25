9285b39b0e62425bc47f9952b29a06a14ee9d54c
evans <maingibiz002@gmail.com>
2026-07-25 17:28:51 +0300

    htpp response check

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Package, Transaction, ActiveSession
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 def home_view(request):
     """Renders the personal homepage with services."""
