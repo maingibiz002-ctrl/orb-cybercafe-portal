3253094a4fb6c6906e5a30bb0226f2d25298aec1
evans <maingibiz002@gmail.com>
2026-07-27 09:19:13 +0300

    import

--- myapp/views.py
+++ myapp/views.py
@@ -7,6 +7,7 @@ from .models import Package, Transaction, ActiveSession
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 from django.contrib import messages
+from .models import Enrollment
 
 
 def home_view(request):
